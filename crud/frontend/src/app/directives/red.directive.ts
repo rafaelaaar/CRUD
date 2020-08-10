62efeecf0b1e7c62d87f2cdc9b893be60c13d560
Rafaela Araujo <rafaela.aa@gmail.com>
2020-08-10 12:29:57 -0300

    diretivas

---
+++ crud/frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef } from '@angular/core';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+
+@Directive({
+  selector: '[appRed]'
+})
+export class RedDirective {
+
+  constructor(private el:ElementRef) { 
+    el.nativeElement.style.color = '#e35e6b'
+  }
+
+}
