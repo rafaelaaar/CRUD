c3cab5b03346ca2f20284d1d51743e91108d00ea
Rafaela Araujo <rafaela.aa@gmail.com>
2020-08-13 11:02:47 -0300

    CRUD finalizado

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
-import { componentFactoryName } from '@angular/compiler';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
 
 
 const routes: Routes = [{
@@ -12,7 +15,17 @@ const routes: Routes = [{
 },{
   path:"products",
   component: ProductCrudComponent
-}];
+},{
+  path:"products/create",
+  component: ProductCreateComponent
+}, {
+  path:"products/update/:id",
+  component: ProductUpdateComponent
+}, {
+  path:"products/delete/:id",
+  component: ProductDeleteComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
